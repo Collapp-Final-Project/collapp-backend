816a35f547ea08dccffe9c5708e28fc8e23fdf56
atapiamallea <a.tapia.mallea@gmail.com>
2026-07-22 23:24:06 +0200

    fix: correct existsByEmail typo

--- src/main/java/com/collapp/project/repository/UserRepository.java
+++ src/main/java/com/collapp/project/repository/UserRepository.java
@@ -10,6 +10,6 @@ public interface UserRepository extends JpaRepository<User,Long>{
     
     Optional<User> findByEmail(String email);
 
-    boolean exiexistsByEmail(String email);
+    boolean existsByEmail(String email);
 
 }
