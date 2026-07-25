2c13a8f42d6a58b5759e731a488936015a26c9ce
atapiamallea <a.tapia.mallea@gmail.com>
2026-07-25 02:31:43 +0200

    fix: switch login authentication from username to email

--- src/main/java/com/collapp/project/dto/auth/LoginRequest.java
+++ src/main/java/com/collapp/project/dto/auth/LoginRequest.java
@@ -4,8 +4,8 @@ import jakarta.validation.constraints.NotBlank;
 
 public record LoginRequest(
 
-        @NotBlank(message = "El username es obligatorio")
-        String username,
+        @NotBlank(message = "El email es obligatorio")
+        String email,
 
         @NotBlank(message = "La contraseña es obligatoria")
         String password
